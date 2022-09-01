519acb511c792a5590654c5f4ec0e3c10e4b10ce
Zane Fink <zanefink@Zanes-MacBook-Pro.local>
2022-08-31 20:37:23 -0400

    test 2

--- server.js
+++ server.js
@@ -70,7 +70,12 @@ app.delete('/state_hikes/:id', (req, res) => {
 // Image Array update / Put
 //////////////////////////////////////////////////////////////
 app.put('/state_hikes/:id', (req, res) => {
-    Hikes.findByIdAndUpdate(req.params.id, req.body.imageArray.split(','), {new:true}, (err, updateHike) => {
+    Hikes.findByIdAndUpdate(req.params.id,{
+      
+    imageArray: req.body.imageArray.split(',')
+
+    },
+    {new:true}, (err, updateHike) => {
       res.json(updateHike)
     })
 });
